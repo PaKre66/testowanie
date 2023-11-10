c90b9b0a9cb7e48d0e1aed432006cb7dadf551e5
PaKre66 <113010144+PaKre66@users.noreply.github.com>
2023-11-10 09:15:45 +0100

    robocik z animacją
    jebany rabbit hole

--- projekt test/script.js
+++ projekt test/script.js
@@ -5,16 +5,33 @@ document.addEventListener('DOMContentLoaded', function () {
   
     toggleButton.addEventListener('click', function () {
       const asideWidth = 250; // Szerokość aside w pikselach
-  
+      
       if (article.style.marginRight === `${asideWidth}px`) {
         // Jeśli aside jest już wysunięte, schowaj je
         article.style.marginRight = '0';
+        article.style.transition='margin-right 1s ease-in-out';
         aside.style.marginRight = `-${asideWidth}px`;
+        // aside.style.display='none';
+        // var chowanie=document.getElementById('article');
+        // chowanie.addEventListener('animationed',function(){
+        //   console.log('animacja zakończona');
+        //   aside.style.display='none';
+        // });
+        
       } else {
         // Jeśli aside jest schowane, wysuń je
         article.style.marginRight = `${asideWidth}px`;
         aside.style.marginRight = '0';
+        // aside.style.display='block';
+        aside.style.transition='margin-right 1s ease-in-out';
+        // var wysuwanie=document.getElementById('aside');
+        // toggleButton.addEventListener('click',function(){
+        //   aside.style.display='block';
+        //   aside.style.transition='margin-right 1s ease-in-out';
+        // });
       }
     });
   });
+  // trzeba dodać addEventListener żeby wykrywał koniec 
+  // animacji i tam display none przy schowanym a przy wysuniętym block
   
